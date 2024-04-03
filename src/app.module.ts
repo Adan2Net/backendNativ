888cf62978fb2b77d27028398a753fe82dedc523
Adan2Net <adaneduardo3015228321@gmail.com>
2024-04-02 23:36:42 -0500

    endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { FlightsModule } from './flights/flights.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, BookingsModule, FlightsModule],
   controllers: [AppController],
   providers: [AppService],
 })
